35068a154e2bff3d315df33c050194763ddc4226
hslape <awinter71417@gmail.com>
2024-03-11 20:18:27 -0500

    Add imports and routes for login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ItemComponent } from './item/item.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  // standalone: true,
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  // imports: [ItemComponent],
+  imports: [ItemComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'phatloot_client';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,2 +1,10 @@
-// import { Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 // import { authGuard } from './auth.guard';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
+  },
+];
