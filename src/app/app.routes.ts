6273ef97cf10dad2848b1b73700f830accd1c4d7
hslape <awinter71417@gmail.com>
2024-03-11 20:31:44 -0500

    Fix authentication routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
-// import { authGuard } from './auth.guard';
+import { authGuard } from './auth.guard';
+import { noAuthGuard } from './no-auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
+    canActivate: [noAuthGuard],
+  },
+  {
+    path: '',
+    canActivate: [authGuard],
   },
 ];
