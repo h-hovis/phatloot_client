191c9251d46c3df9e2e2c99d71e983304c76c214
hslape <awinter71417@gmail.com>
2024-03-18 20:02:35 -0500

    Add Google Fonts and update navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ItemComponent } from './item/item.component';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  imports: [ItemComponent, RouterOutlet],
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [ItemComponent, RouterOutlet, NavbarComponent]
 })
 export class AppComponent {
   title = 'phatloot_client';
