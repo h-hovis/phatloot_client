25cc29a68895fe207af4fc283a52872ea3109982
hslape <awinter71417@gmail.com>
2024-03-27 12:42:18 -0500

    Update home component to display a product list

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
+import { ProductListComponent } from 'src/app/product-list/product-list.component';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit{
+  homeProducts: Product[] = [];
+
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
