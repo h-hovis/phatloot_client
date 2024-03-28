f4794553d629482b9cf53be87e529147a085871f
hslape <awinter71417@gmail.com>
2024-03-28 18:39:38 -0500

    Refactor app component to include home component and product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { ProductComponent } from './product/product.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./features/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [ProductComponent, RouterOutlet, NavbarComponent]
+    imports: [ProductComponent, RouterOutlet, NavbarComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'phatloot_client';
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { ProductListComponent } from 'src/app/product-list/product-list.component';
 import { Product } from 'src/app/models/product';
+import { NavbarComponent } from "../../navbar/navbar.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [ProductListComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [ProductListComponent, NavbarComponent]
 })
 export class HomeComponent implements OnInit{
   homeProducts: Product[] = [];
