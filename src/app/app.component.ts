cae495a6b6dec5f4df412d04dde22475cd1d9551
hslape <awinter71417@gmail.com>
2024-04-04 20:07:58 -0500

    Add ProductListComponent to app.component.ts and app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { ProductComponent } from './product/product.component';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HomeComponent } from "./features/home/home.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [ProductComponent, RouterOutlet, NavbarComponent, HomeComponent]
+    imports: [ProductComponent, RouterOutlet, NavbarComponent, HomeComponent, ProductListComponent]
 })
 export class AppComponent {
   title = 'phatloot_client';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
+import { AppComponent } from './app.component';
 import { ProductComponent } from "./product/product.component";
 
 @NgModule({
     declarations: [
-        // AppComponent
+
     ],
     providers: [],
     bootstrap: [],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ProductComponent
+        ProductComponent,
+        AppComponent
     ]
 })
 export class AppModule { }
